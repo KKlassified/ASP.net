dbcdd5950e50ed95f53b1464b88205c8ecb71dee
Kyle Kasner <49010179+KKlassified@users.noreply.github.com>
2020-04-17 21:23:24 -0700

    Update UpdateInfo.aspx.cs

--- UpdateInfo.aspx.cs
+++ UpdateInfo.aspx.cs
@@ -19,7 +19,7 @@ namespace PracticemakesPerfect
 
 
                 
-                    string connStr = "server= KING-KBK\\SQLEXPRESS2014; Database=Prog117DB; user id=PROG117_web_user; Password=abc123;";
+                    string connStr = "server= SQLEXPRESS2014; Database=Prog117DB; user id=PROG117_web_user; Password=PASSWORD;";
                     SqlConnection conn = new SqlConnection(connStr);
                     conn.Open();
 
